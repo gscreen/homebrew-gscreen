86c748f97789300a070dbf7266d2281904c8db82
Timothy Andrew <mail@timothyandrew.net>
2022-10-15 13:00:49 +0100

    release 0.5.0

--- Formula/gscreen.rb
+++ Formula/gscreen.rb
@@ -10,14 +10,14 @@ class Gscreen < Formula
 
   desc "Slideshows across all your Google Photos media"
   homepage "https://photoscreensaver.app"
-  version "0.4.1"
+  version "0.5.0"
   url "https://gscreen-releases.s3.amazonaws.com/v#{version}/gscreen-macos-#{arch}.bin.tar.gz"
 
   @@filename = "gscreen-macos-#{arch}.bin"
 
   sha256 case arch
-  when "amd64" then "ad5d804d35ab47752e5412ac173328fe44b48f77f4d6e33f95d53d98e3b768ca"
-  when "arm64" then "d37d4618f2a40b53ade51c2374667a409fb4f128a5cb1113a2d66e35be981f3b"
+  when "amd64" then "c4fc41a001fc050961982997301ca6235ef30bd5e6eaf90eca944ec1df3f1b60"
+  when "arm64" then "b0b154e0911435fb6695dde4b887c566955148d966fa702871d7abc6e7d4b104"
   else
     raise "gScreen: Unsupported system #{arch}"
   end
